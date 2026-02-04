c7c3ac57dd66d6bd2753cc774e4aeab703fb3f34
Siva prasad <sivaprasad3150@gmail.com>
2026-02-04 18:56:31 +0530

    shell-script

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -7,5 +7,6 @@ echo "current directory of the script:$PWD"
 echo "Owner of the script:$USER"
 echo "Home Directory of the user:$HOME"
 echo "process id of the script:$$"
+sleep 50 &
 echo "process id of the last executed command:$!"
 
