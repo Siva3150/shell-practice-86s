76d3766d85f0f6593533da1a49e1a8857852df91
Siva prasad <sivaprasad3150@gmail.com>
2026-02-04 18:51:05 +0530

    shell-script

---
+++ 08-special-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+echo "All variables passed to the script:$@" 
+echo "All variables passed to the script:$*"
+echo "script name:$0
+echo "current directory of the script:$PWD"
+echo "Owner of the script:$USER"
+echo "Home Directory of the user:$HOME"
+echo "process id of the script:$$"
+
