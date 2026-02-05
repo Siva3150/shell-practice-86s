bf3bfa18c026458edcde58e07a7b3e50a8ffbf61
Siva prasad <sivaprasad3150@gmail.com>
2026-02-05 13:10:59 +0530

    shell-script

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -4,9 +4,9 @@ echo "please enter your number:"
 read NUMBER 
 
 if [ $(($NUMBER % 2)) -eq 0 ];  then 
-echo "Given number: $NUMBER is even"
+    echo "Given number: $NUMBER is even"
 
 else 
 
-echo "Given number: $NUMBER is odd"
+    echo "Given number: $NUMBER is odd"
 fi
