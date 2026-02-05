9ade389823eeb985fce6cf981b73eb510db7255b
Siva prasad <sivaprasad3150@gmail.com>
2026-02-05 13:00:47 +0530

    shell-script

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -3,7 +3,9 @@
 NUMBER=$1
 
 if [ $NUMBER -gt 10 ]; then 
-echo "The given number: $NUMBER is greater than 10"
+   echo "The given number: $NUMBER is greater than 10"
+elif [ $NUMBER -eq 10 ]; then
+   echo "The given number: $NUMBER is equal to 10
 else 
-echo "The given number: $NUMBER is less than 10"
+    echo "The given number: $NUMBER is less than 10"
 fi 
