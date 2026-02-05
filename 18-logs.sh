6fbd1bcaf27224589f3fd7fd648517bcd56c88c0
Siva prasad <sivaprasad3150@gmail.com>
2026-02-05 14:27:02 +0530

    shell-script

--- 18-logs.sh
+++ 18-logs.sh
@@ -8,3 +8,6 @@ USERID=$(id -u)
 
 LOGS_FOLDER="/var/log/shell-script-logs"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #/var/log/shell-script-logs/18-logs.log
+
+mkdir $LOGS_FOLDER
