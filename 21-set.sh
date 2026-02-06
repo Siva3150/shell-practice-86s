461cacdea107cea8354c2d7c05953258e9cff497
Siva prasad <sivaprasad3150@gmail.com>
2026-02-06 19:14:38 +0530

    shell-script

--- 21-set.sh
+++ 21-set.sh
@@ -6,7 +6,7 @@ error(){
     echo "There is an error in $LINENO, Command is: $BASH_COMMAND"
 }
 
-#trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
 
 echo "Hello.."
 echo "Before error.."
