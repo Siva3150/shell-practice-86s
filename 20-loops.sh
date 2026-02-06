5b18e67aa4c118102d468b797654e50952854c20
Siva prasad <sivaprasad3150@gmail.com>
2026-02-06 16:50:57 +0530

    shell-script

--- 20-loops.sh
+++ 20-loops.sh
@@ -40,7 +40,7 @@ do
   # if exit status is 0, already installed. -ne 0 need to install it
   if [ $? -ne 0 ]; then 
     dnf install $package -y &>>$LOG_FILE
-    validate $? "$package"
+    VALIDATE $? "$package"
   else 
      echo -e " $package already exists $Y skipping $N" | tee -a $LOG_FILE
   fi 
